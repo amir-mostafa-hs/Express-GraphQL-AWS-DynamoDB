22983b960a259ad0dbde06ce6d392852647f47c3
amir-mostafa-hs <amir-mostafa-hs@outlook.com>
2022-11-18 02:07:55 +0330

    add base file

---
+++ test/express-graph_ql-aws-dynamo_db.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const ExpressGraphQlAwsDynamoDb = require('../lib/express-graph_ql-aws-dynamo_db-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/express-graph_ql-aws-dynamo_db-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new ExpressGraphQlAwsDynamoDb.ExpressGraphQlAwsDynamoDbStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
